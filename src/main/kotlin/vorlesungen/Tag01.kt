9a5f6098945422dc6d9919a1f5a99e7261a5fb6c
MarianneSyntax <113111255+MarianneSyntax@users.noreply.github.com>
2023-10-16 13:50:42 +0100

    Tag01: println, einrueckungen, kommentare

--- src/main/kotlin/vorlesungen/Tag01.kt
+++ src/main/kotlin/vorlesungen/Tag01.kt
@@ -2,4 +2,39 @@ package vorlesungen
 
 fun main() {
 
+    // Code Kommentar
+
+    /*
+        zweizeiliger Code Kommentar
+        kann hier enter druecken
+        usw.
+     */
+
+
+    // LiveBeispiel 1: String in Konsole drucken
+    println("Halloefj aieg h;eigh ;aieglhk ;sle b124603285620495862974#&%#&^#%!*^&#^*&!^%#*& Welt")
+    println("Wie geht's?")
+    println("Hallo Welt !")
+
+    println("Hey")
+    println("Ho")
+    println("Let's go")
+
+    // Bonus:
+    // LiveBeispiel 2: String in die Konsole einlesen, im Programm benutzen
+
+
+}
+
+fun bla(){
+    println("blablaalb")
+    println("blablaalb")
+    println("blablaalb")
+    println("blablaalb")
+    println("blablaalb")
+    println("blablaalb")
+    println("blablaalb")
+    println("blablaalb")
+    println("blablaalb")
+
 }
