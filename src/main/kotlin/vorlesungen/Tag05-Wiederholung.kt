12ba18d31b86dcbf6aa47f0f1c55aa6b8c7a7b96
MarianneSyntax <113111255+MarianneSyntax@users.noreply.github.com>
2023-10-20 12:36:29 +0100

    Tag05: Wiederholung Woche 1

---
+++ src/main/kotlin/vorlesungen/Tag05-Wiederholung.kt
@@ -0,0 +1,17 @@
+package vorlesungen
+
+fun main() {
+    var x: Double = 9.0
+
+    // println() Nutzung: druckt in die Konsole aus
+    println(x)
+    println("Hallo wie geht's")
+    println(10)
+
+
+    // readln() Nutzung: nimmt Daten, die man in die Konsole eingibt, ins Programm, indem wir sie in eine Variable speichern
+    println("Bitte gib dein Alter ein...")
+    var alter: String = readln()
+    println("Ok, ein Alter ist $alter Jahre.")
+
+}
