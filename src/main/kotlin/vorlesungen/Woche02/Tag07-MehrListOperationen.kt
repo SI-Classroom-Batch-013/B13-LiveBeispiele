f697b7d1944603819c774cae03eddf9977323147
MarianneSyntax <113111255+MarianneSyntax@users.noreply.github.com>
2023-10-31 13:53:18 +0000

    wieder einkommentiert

--- src/main/kotlin/vorlesungen/Woche02/Tag07-MehrListOperationen.kt
+++ src/main/kotlin/vorlesungen/Woche02/Tag07-MehrListOperationen.kt
@@ -2,104 +2,103 @@ package vorlesungen.Woche02
 
 fun main(){
 
-//    // liste anlegen: ShoppingList
+    // liste anlegen: ShoppingList
     val shoppingList: List<String> = listOf("Apfel", "Banane", "Brokkoli", "Kuchen", "Kekse", "Kaffee", "Tee")
-//    // shoppingList.add()
-//
-//
-//    // letztes Element: 3 Varianten
-//    // alternative zum Inhalt des letzten index: last()
-//    var lastElement: String = shoppingList.last() // shoppingList.last = ohne Klammern, last ist eine Eigenschaft. mit Klammern: Funktion
-//    var last: String = shoppingList[shoppingList.size-1]
-//    var letztesElement: String = shoppingList[shoppingList.lastIndex]
-//    var lastElem: String = shoppingList.get(shoppingList.lastIndex) // shoppingList.size-1
-//
-//    var lastIndex: Int = shoppingList.size-1 // 6
-//
-//    // liste in mutable (veränderbare) liste umwandeln, in neuer variable speichern:
+    // shoppingList.add()
+
+
+    // letztes Element: 3 Varianten
+    // alternative zum Inhalt des letzten index: last()
+    var lastElement: String = shoppingList.last() // shoppingList.last = ohne Klammern, last ist eine Eigenschaft. mit Klammern: Funktion
+    var last1: String = shoppingList[shoppingList.size-1]
+    var letztesElement: String = shoppingList[shoppingList.lastIndex]
+    var lastElem: String = shoppingList.get(shoppingList.lastIndex) // shoppingList.size-1
+
+    var lastIndex: Int = shoppingList.size-1 // 6
+
+    // liste in mutable (veränderbare) liste umwandeln, in neuer variable speichern:
     val mutableShoppingList: MutableList<String> = shoppingList.toMutableList()
-//    //shoppingList.add() // geht immernoch nicht, shoppingList wird nicht bearbeitet, es wird nur eine mutable Kopie davon zurueckgegeben.
-//    mutableShoppingList.add("Kaugummi")
-//
-//
-//
-//// LESENDE OPERATIONEN: AUF LISTS UND MUTABLE LISTS
-//
-//    // size, also Länge der Liste, in variable speichern
-//    var shoppingSize: Int = shoppingList.size
-//
-//    println()
-//
-//
-//    // index vs size:
-//    // size in lastIndex speichern
-//    var lastIndex2: Int = shoppingSize-1
+    //shoppingList.add() // geht immernoch nicht, shoppingList wird nicht bearbeitet, es wird nur eine mutable Kopie davon zurueckgegeben.
+    mutableShoppingList.add("Kaugummi")
+
+
+
+// LESENDE OPERATIONEN: AUF LISTS UND MUTABLE LISTS
+
+    // size, also Länge der Liste, in variable speichern
+    var shoppingSize: Int = shoppingList.size
+
+    println()
+
+
+    // index vs size:
+    // size in lastIndex speichern
+   var lastIndex2: Int = shoppingSize-1
 //
 //
 //    // alternative zu shoppingList[0]: first()
-//    var firstElement: String = shoppingList.first()
-//    var firstElem: String = shoppingList[0]
-//
-//
-//    // min: gibt "kleinstes" Element raus
-//    var min: String = shoppingList.min()
-//
-//    // max gibt "größtes" Element raus
-//    var max: String = shoppingList.max()
-//
-//    var intList: List<Int> = listOf(2,4,66,33,898,1)
-//    // min: gibt "kleinstes" Element raus
-//    var minInt: Int = intList.min()
-//
-//    // max gibt "größtes" Element raus
-//    var maxInt: Int = intList.max()
-//
-//
-//
-//    // indexOf() - erstes Vorkommen eines Elements in der Liste
-//    // Vorbereitung: Ein Element doppelt in die Liste fügen
-//    println("IndexOf:")
-//    mutableShoppingList.add("Banane")
-//    var bananaIndex: Int = mutableShoppingList.indexOf("Banane")
-//
-//
-//    // lastIndexOf() - letztes Vorkommen eines Elements in der Liste
-//    var lastBananaIndex: Int = mutableShoppingList.lastIndexOf("Banane")
-//
-//    // contains: gibt true zurück, wenn das gesuchte Element in der Liste ist
-//    var containsGum: Boolean = mutableShoppingList.contains("Kaugummi") // true
-//
-//
-//    // containsAll : siehe contains, aber: nicht ein einzelnes Element, sondern ganze Liste
-//    var containsBananaApple: Boolean = mutableShoppingList.containsAll(listOf("Banane","Apfel")) // true
-//
-//    // subList: gibt liste vom 1. index in den Klammern bis AUSschliesslich den 2. Index aus
-//
-//    println("Sublist: ")
-//    val lastThreeItems: List<String> = mutableShoppingList.subList(mutableShoppingList.size-3,mutableShoppingList.size) // ist EXklusive dem 2. Wert
-//    println(lastThreeItems)
-//
-//
-//    // slice: kann eine Int Range (x..y) oder eine Liste übergeben bekommen, das obere Ende der Range ist INklusive EINbegriffen (nicht wie bei subList)
-//    println("Slice mit IntRange:")
-//    var intRange: IntRange = 1..100
-//    //println(intRange)
-//    var sliced: List<String> = mutableShoppingList.slice(0..3)
-//    println(sliced)
-//
-//
-//
-//    // reversed: erstellt eine Kopie der shoppingList, kehrt darin die Reihenfolge um
-//    println("Reversed:")
-//    var reversedShoppingList: List<String> = shoppingList.reversed()
-//
-//    // shuffled: erstellt eine Kopie der shoppingList, mischt sie durch
-//    println("Shuffled:")
-//    var shuffledShoppingList: List<String> = shoppingList.shuffled()
-//
-//    println("Sorted: ")
-//    var sortedShoppingList: List<String> = shoppingList.sorted()
-//
+    var firstElement: String = shoppingList.first()
+    var firstElem2: String = shoppingList[0]
+
+
+    // min: gibt "kleinstes" Element raus
+    var min: String = shoppingList.min()
+
+    // max gibt "größtes" Element raus
+    var max: String = shoppingList.max()
+
+     var intList: List<Int> = listOf(2,4,66,33,898,1)
+     // min: gibt "kleinstes" Element raus
+    var minInt: Int = intList.min()
+
+    // max gibt "größtes" Element raus
+    var maxInt: Int = intList.max()
+
+
+
+    // indexOf() - erstes Vorkommen eines Elements in der Liste
+    // Vorbereitung: Ein Element doppelt in die Liste fügen
+    println("IndexOf:")
+    mutableShoppingList.add("Banane")
+    var bananaIndex: Int = mutableShoppingList.indexOf("Banane")
+
+
+    // lastIndexOf() - letztes Vorkommen eines Elements in der Liste
+    var lastBananaIndex: Int = mutableShoppingList.lastIndexOf("Banane")
+
+    // contains: gibt true zurück, wenn das gesuchte Element in der Liste ist
+    var containsGum: Boolean = mutableShoppingList.contains("Kaugummi") // true
+
+
+    // containsAll : siehe contains, aber: nicht ein einzelnes Element, sondern ganze Liste
+    var containsBananaApple: Boolean = mutableShoppingList.containsAll(listOf("Banane","Apfel")) // true
+
+    // subList: gibt liste vom 1. index in den Klammern bis AUSschliesslich den 2. Index aus
+    println("Sublist: ")
+    val lastThreeItems: List<String> = mutableShoppingList.subList(mutableShoppingList.size-3,mutableShoppingList.size) // ist EXklusive dem 2. Wert
+    println(lastThreeItems)
+
+
+    // slice: kann eine Int Range (x..y) oder eine Liste übergeben bekommen, das obere Ende der Range ist INklusive EINbegriffen (nicht wie bei subList)
+    println("Slice mit IntRange:")
+    // Beispiel für eine IntRange: hier sind die Zahlen 1 bis inklusive 100 enthalten
+    var intRange: IntRange = 1..100
+    //println(intRange)
+    var sliced: List<String> = mutableShoppingList.slice(0..3)
+    println(sliced)
+
+
+    // reversed: erstellt eine Kopie der shoppingList, kehrt darin die Reihenfolge um
+    println("Reversed:")
+    var reversedShoppingList: List<String> = shoppingList.reversed()
+
+    // shuffled: erstellt eine Kopie der shoppingList, mischt sie durch
+    println("Shuffled:")
+    var shuffledShoppingList: List<String> = shoppingList.shuffled()
+
+    println("Sorted: ")
+    var sortedShoppingList: List<String> = shoppingList.sorted()
+
 
 
 // SCHREIBENDE OPERATIONEN: NUR MIT MUTABLE LISTS
