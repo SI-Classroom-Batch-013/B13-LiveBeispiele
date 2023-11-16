f339737ce00a9d376552341762ad61b52aacdb4f
GordonLucasSyn <113020395+GordonLucasSyn@users.noreply.github.com>
2023-11-16 10:44:37 +0100

    Tag 19: String.format und Datums Formatieren

--- src/main/kotlin/vorlesungen/Woche04/Tag19Klammern/StringFormat.kt
+++ src/main/kotlin/vorlesungen/Woche04/Tag19Klammern/StringFormat.kt
@@ -1,2 +1,31 @@
 package vorlesungen.Woche04.Tag19Klammern
 
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+
+fun main(){
+
+    var preis = 4.68773427656253
+
+    val zahl = 10.0/3
+
+    var stringFormat = "%.2f"
+
+
+    println(stringFormat.format(preis))
+    println(stringFormat.format(zahl))
+    println(stringFormat)
+
+
+    var aktuelleUhrzeit = LocalDateTime.now()
+//    println("%t".format(aktuelleUhrzeit))
+
+    // wir brauchen ein "Formatierer" Objekt
+    val formatter = DateTimeFormatter.ofPattern("dd.MM. HH:mm:ss")
+    val formatiertesDatum = aktuelleUhrzeit.format(formatter)
+
+    println(formatiertesDatum)
+
+
+}
