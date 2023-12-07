cb7d8060ce4819dbfdf0f7f92558f0ce2e2586d7
MarianneSyntax <113111255+MarianneSyntax@users.noreply.github.com>
2023-12-07 09:23:45 +0100

    main in PokeMain ausgelagert,
    main schlanker gemacht: aktionen, gameOver und Spielrunde in funktionen ausgelagert

--- src/main/kotlin/vorlesungen/Woche07Wiederholung/PokeMain.kt
+++ src/main/kotlin/vorlesungen/Woche07Wiederholung/PokeMain.kt
@@ -1,6 +1,5 @@
 package vorlesungen.Woche07Wiederholung
 
-import Beutel
 import Pokemon
 
 fun main() {
@@ -16,10 +15,9 @@ fun main() {
 
     // Team an Helden
     val helden: MutableList<Pokemon> = mutableListOf(bisasam,schiggy,glumanda)
-    // Team an Gegnern
+    // Team an Gegner
     val gegnerListe: MutableList<Pokemon> = mutableListOf(taubsi,rattfratz,habitak)
 
-    // Game Loop
     spielRunde(helden, gegnerListe)
 
 }
@@ -30,96 +28,74 @@ fun spielRunde(helden: MutableList<Pokemon>, gegnerListe: MutableList<Pokemon>) 
     var gameOver: Boolean = false
     // Rundencounter
     var round: Int = 1
-    var teamBeutel = Beutel()
 
 
     while (!gameOver){
         println("---Runde $round!---")
-        if (round == 2){
-            println("Das Gegner Team spawnt in der 2. Runde einen Helfer...")
-            gegnerListe.add(Pokemon("Helfer-Pokemon Nidoran"))
-        }
         // print alle helden in meinem team und ihre aktionen --> ueber liste der helden iterieren
         println("--Unser Team:--")
         // print alle helden in meinem team und ihre aktionen --> ueber liste der helden iterieren
         helden.forEach { println(it) }
         // print alle gegner --> ueber liste der gegner iterieren
         println("--Gegner Team:--")
-        var lebendeGegner = gegnerListe.filter { !it.isDead } // nur lebende Gegner werden rausgefiltert
-        lebendeGegner.forEach { println(it) }
+        var lebendeGegner = gegnerListe.filter { !it.isDead }.toMutableList() // nur lebende Gegner werden rausgefiltert
 
-        // schleife, bis alle helden angegriffen haben:
-        // print: "1./2./3. held soll angreifen, welche attacke?"
-        lebendeGegner = heldenAngriff(helden, lebendeGegner,teamBeutel)
+
+        lebendeGegner = aktionen(helden, lebendeGegner)
         // gegner greifen an: exact das gleiche
         // hardcode, keine richtige Logik
-        println("Taubsi/Rattfratz/Habitak greift an! Bisasam/Glumanda/Schiggy verliert x hp....")
+        println("Taubsi greift an! Bisasam verliert x hp....")
+        // lebendeHelden = aktionen(lebendeGegner, helden)
 
         println("Runde $round beendet!")
-        println("ggf. nochmal Status von allen ausdrucken...")
-
-
-        // pruefen, ob noch helden oder gegner uebrig sind
+        println("ggf. Status von allen ausdrucken...")
         gameOver = gameOver(helden, lebendeGegner, gameOver)
         round++
     }
-    println("Game Loop beendet!") // Logik: helden oder gegner tot?
-
+    println("Spiel beendet! Alle Gegner sind besiegt!") // Logik: helden oder gegner tot?
 
 }
 
-fun gameOver(helden: List<Pokemon>, lebendeGegner: List<Pokemon>,
-    gameOver: Boolean
-): Boolean {
+private fun gameOver(helden: MutableList<Pokemon>, lebendeGegner: MutableList<Pokemon>, gameOver: Boolean): Boolean {
     var gameOver1 = gameOver
     if (helden.isEmpty()) {
-        gameOver1 = true
         println("Unsere Helden haben versagt... Game Over :(")
-    } else if (lebendeGegner.isEmpty()){
         gameOver1 = true
-        println("Geschafft! Unsere Helden haben gewonnen, Game Over :)")
+    } else if (lebendeGegner.isEmpty()){
+        println("Geschafft! Unsere Helden haben gewonnen! Game Over :) ")
+        gameOver1 = true
     }
     return gameOver1
 }
 
-private fun heldenAngriff(
-    helden: List<Pokemon>,
-    lebendeGegner: List<Pokemon>, teamBeutel: Beutel
-): List<Pokemon> {
+private fun aktionen(helden: MutableList<Pokemon>, lebendeGegner: MutableList<Pokemon>): MutableList<Pokemon> {
     var lebendeGegner1 = lebendeGegner
+    var inputValid = false
+    // schleife, bis alle helden angegriffen haben:
+    // print: "1./2./3. held soll angreifen, welche attacke?"
     for (held in helden) {
-        var validInput = false
-        while (!validInput) {
-            println("${held.name} soll angreifen. Wähle die Attacke per Zahleneingabe aus!")
-            println("[1] => Tackle, [2] => Heuler, etc, [4] => Beutel nutzen")
+        println("${held.name} soll angreifen. Wähle die Attacke per Zahleneingabe aus!")
+        while (!inputValid){
+            println("[1] => Tackle, [2] => Heuler, etc")
             try {
                 val choice = readln().toInt()
                 when (choice) {
                     1 -> {
-                        if (lebendeGegner1.isNotEmpty()) {
-                            held.tackle(lebendeGegner1.first())
-                            // angeben, dass ggf. der Gegner bereits gestorben ist
-                            lebendeGegner1 = lebendeGegner1.filter { !it.isDead }
-                        } else {
-                            println("Es gibt keine lebenden Gegner mehr.")
-                        }
-                        validInput = true
+                        held.tackle(lebendeGegner1.first())
+                        // angeben, dass ggf gegner bereits gestorben ist
+                        lebendeGegner1 = lebendeGegner1.filter { !it.isDead }.toMutableList()
+                        inputValid = true
                     }
                     // 2 -> held.heuler(gegner.random())
-                    // etc weitere Attacken
-                    4 -> {
-                        println("Du willst also den Beutel nutzen.")
-                        var beutelEingabe = held.useBeutel(teamBeutel)
-                        if (beutelEingabe != 3) {
-                            validInput = true
-                        }
-                    }
+                    // etc weitere attacken
                     else -> {
-                        println("Falsche Eingabe, versuche es nochmal...")
+                        println("Falsche Zahl eingegeben, gib eine gültige Zahl ein!")
+                        // inputValid bleibt hier false -> Schleife geht von vorne los
                     }
+
                 }
-            } catch (e: NumberFormatException) {
-                println("Ungültige Eingabe! Gib eine Zahl ein...")
+            } catch (e: Exception) {
+                println("Bitte eine Zahl eingeben... Probier's nochmal")
             }
         }
     }
--- src/main/kotlin/vorlesungen/Woche07Wiederholung/Pokemon.kt
+++ src/main/kotlin/vorlesungen/Woche07Wiederholung/Pokemon.kt
@@ -1,4 +1,4 @@
-import vorlesungen.Woche07Wiederholung.roundDouble
+
 
 // Primärer Konstruktor: direkt beim Erstellen der Klasse
 class Pokemon(var name: String, var type: String, var level: Int = 1, var isDead: Boolean = false) {
@@ -9,7 +9,7 @@ class Pokemon(var name: String, var type: String, var level: Int = 1, var isDead
 
     // init Block: da der Primäre Konstruktor keinen eigenen Körper hat (der Körper ist ja einfach die gesamte Klasse), brauchen wir den init Block. Der wird, wie die Körper der sekundären Konstruktoren, immer beim Initialisieren = Erstellen eines konkreten Pokemons aufgerufen
     init {
-        this.hp = 10.0
+        this.hp = 3.0
         this.ep = 0.0
         this.ap = level * 3.0
 
@@ -18,7 +18,7 @@ class Pokemon(var name: String, var type: String, var level: Int = 1, var isDead
         //Thread.sleep(500)
         println("$name hat $ap AP und folgende Attacken:")
         // Thread.sleep(500)
-        println("Tackle (${level * ap} Schaden)")
+        println("Tackle (${level + ap} Schaden)")
         // Thread.sleep(500)
         println("--Primäre Konstruktion beendet--")
 
@@ -26,7 +26,7 @@ class Pokemon(var name: String, var type: String, var level: Int = 1, var isDead
 
 
     // sekundärer Konstruktor: Pokemon ist automatisch vom Typ Normal
-    constructor(name: String): this(name, "Normal") {
+    constructor(name: String) : this(name, "Normal") {
         println("---Sekundärer Konstruktor Call---")
         // Thread.sleep(500)
         println("Ein Pokemon namens $name mit Default den Typ 'Normal' wurde angelegt.")
@@ -41,7 +41,6 @@ class Pokemon(var name: String, var type: String, var level: Int = 1, var isDead
         println("${gegner.name} hat vorher ${gegner.hp} HP...")
         println("$name's Angriff triggt ${gegner.name}!")
         gegner.hp -= this.ap
-        gegner.hp = roundDouble(gegner.hp)
         println("${gegner.name} verliert $ap HP und hat noch ${gegner.hp} HP!")
         // Abfangen, dass Gegner direkt stirbt:
         if (gegner.hp <= 0){
@@ -55,77 +54,6 @@ class Pokemon(var name: String, var type: String, var level: Int = 1, var isDead
         return """
             Pokemon: $name
             HP: $hp
-        """.trimIndent()
-    }
-
-    fun useBeutel(beutel: Beutel): Int {
-        var inputValid = false
-        var input = 0
-        println(beutel)
-        println("Was willst du nutzen?")
-        println("[1] Heiltrank (${beutel.anzahlHeilTränke} übrig)")
-        println("[2] Booster (${beutel.anzahlBooster} übrig)")
-        println("[3] Beutel verlassen")
-        while (!inputValid) {
-            try {
-                input = readln().toInt()
-                when (input){
-                    1 -> {
-                        if (beutel.anzahlHeilTränke > 0){
-                            beutel.useHeilTrank(this)
-                        } else {
-                            println("Du hast keine Heiltränke übrig!")
-                            useBeutel(beutel)
-                        }
-                    }
-                    2 -> {
-                        if (beutel.anzahlBooster > 0){
-                            beutel.useBooster(this)
-                        } else {
-                            println("Du hast keine Booster übrig!")
-                            useBeutel(beutel)
-                        }
-                    }
-                    else -> {
-                        println("Ungültige Zahl eingegeben... Versuch's nochmal!")
-                        useBeutel(beutel)
-                    }
-                }
-                inputValid = true
-                return input
-            } catch (e: Exception){
-                println("Ungültige Eingabe, bitte Zahl eingeben!")
-            }
-        }
-        return input
-    }
-}
-
-class Beutel(){
-    var anzahlHeilTränke = 3
-    var anzahlBooster = 2
-
-    fun useHeilTrank(user: Pokemon){
-        println("${user.name} (${user.hp} HP) trinkt Trank...")
-        anzahlHeilTränke--
-        user.hp *= 1.3
-        user.hp = roundDouble(user.hp)
-        println("${user.name} hat jetzt ${user.hp}!")
-    }
-
-    fun useBooster(user: Pokemon){
-        println("${user.name} (${user.ap} AP) trinkt Booster...")
-        anzahlBooster--
-        user.ap *= 1.3
-        user.ap = roundDouble(user.ap)
-        println("${user.name}'s AP wurden um 30% erhöht!")
-    }
-
-    override fun toString():String{
-       return """
-            Beutelinhalt:
-            Tränke: $anzahlHeilTränke
-            Booster: $anzahlBooster
         """.trimIndent()
     }
 }
